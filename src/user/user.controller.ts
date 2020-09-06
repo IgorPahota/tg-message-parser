d48570d991549f89094d3422e2fa0a7c65c31fb1
IgorPahota <eto.pochtovaya@gmail.com>
2020-09-06 22:11:34 +0300

    nest init with dummy user and auth

---
+++ src/user/user.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from "./user.service";
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Get()
+  getHello(): string {
+    return this.userService.getHello()
+  }
+}
